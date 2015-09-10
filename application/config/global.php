2808f4b983a7022b936f2ae58708ca04e13a066c
Duc Ta Minh <ductm1@smartosc.com>
2015-09-10 11:38:24 +0700

    New Code

---
+++ application/config/global.php
@@ -0,0 +1,26 @@
+<?php 
+
+define('CMS_FOLDER', '');
+
+define('CMS_DB_HOST', 'localhost');
+define('CMS_DB_USERNAME', 'root');
+define('CMS_DB_PASSWORD', '');
+define('CMS_DB_NAME', 'cms_duc_hung');
+define('CMS_DB_PREFIX', 'cms_');
+
+
+//Define Link
+define('CMS_DOMAIN', 'http://shoponline.local');
+define('CMS_BASE_URL', 'http://shoponline.local');
+define('CMS_LOGIN_BACKEND_URL', 'admin/auth/login');
+define('CMS_DEFAULT_BACKEND_URL', 'admin');
+
+define('CMS_UPLOAD_FOLDER', '/public/');
+
+//Define User
+define('CMS_SUPER_ADMIN_ID', 1);
+define('CMS_ADMINISTRATOR_EMAIL', 'minhducck@gmail.com');
+define('CMS_DEFAULT_USERGROUP_ID', 2);
+
+define('CMS_ITEM_PER_PAGE', 10);
+
---
+++ template/FeaturedITEM.php
@@ -0,0 +1,19 @@
+<div class="item">
+    <div class="featured-item">
+        <a href="#">
+            <img class="featured-product-img" alt="Hang Hot" title="Hang kinh" src="images/Sail_on_Beach_Backpack-254x402.jpg"/>
+        </a>
+        
+        <div class="col-md-12 featured-product-info">
+            <h3 class="featured-product-title"><a href="#">Product TITLE</a></h3>
+            <div class="featured-product-data col-md-12">
+                <div class="col-sm-6">
+                    <h3 class="featured-product-price">$ 250</h3>
+                </div>
+                <div class="col-sm-6 text-right">
+                    <a href="#" class="featured-product-cart"><span class="glyphicon glyphicon-shopping-cart"></span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
